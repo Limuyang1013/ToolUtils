697824b12d596f79997dd00e55bc1d215adf9020
Limuyang <lin794653318@gmail.com>
2017-02-08 16:55:24 +0800

    Fragment懒加载封装类

---
+++ app/src/main/java/com/muyang/toolutils/common/MyFragment.java
@@ -0,0 +1,39 @@
+package com.muyang.toolutils.common;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.muyang.toolutils.R;
+
+/**
+ * Created by Limuyang on 2017/2/8.
+ * 使用博客地址 http://www.jianshu.com/p/311c7ffdb85b
+ */
+
+public class MyFragment extends ViewPagerFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (rootView==null) {
+            rootView = inflater.inflate(R.layout.activity_main, container, false);
+        }
+        return rootView;
+    }
+
+    @Override
+    protected void onFragmentVisibleChange(boolean isVisible) {
+        super.onFragmentVisibleChange(isVisible);
+        if (isVisible){
+            //   do things when fragment is visible
+            //   if (ListUtils.isEmpty(mDataList) && !isRefreshing()) {
+            //   setRefresh(true);
+            //   loadServiceData(false);
+        }else{
+            //        setRefresh(false);
+        }
+    }
+}
